9ecc35e38226aa0757f8f6ffa166f3ec74d29cca
Marc MacLeod <marbemac+gh@gmail.com>
2024-12-05 16:13:43 -0500

    update example to surface exports bug
    Signed-off-by: Marc MacLeod <marbemac+gh@gmail.com>

--- examples/nano-jsx/routes/[...path].tsx
+++ examples/nano-jsx/routes/[...path].tsx
@@ -1,7 +1,13 @@
 import { defineEventHandler } from "h3";
 import { h, renderSSR } from "nano-jsx";
 
+import { atom, useAtomValue } from "@zedux/react";
+
+const countAtom = atom("counter", 0);
+
 export default defineEventHandler(() => {
-  const html = renderSSR(() => <h1>Nitro + nano-jsx works!</h1>);
+  const count = useAtomValue(countAtom);
+
+  const html = renderSSR(() => <h1>Nitro + nano-jsx works! {count}</h1>);
   return html;
 });
